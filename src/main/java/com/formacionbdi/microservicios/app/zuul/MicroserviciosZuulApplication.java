fbe36720dfa16210a4594baa9731da70d8da99aa
JlFerrete <joseluisferrete@gmail.com>
2020-07-08 13:34:50 +0200

    Initial commit

---
+++ src/main/java/com/formacionbdi/microservicios/app/zuul/MicroserviciosZuulApplication.java
@@ -0,0 +1,13 @@
+package com.formacionbdi.microservicios.app.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviciosZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviciosZuulApplication.class, args);
+	}
+
+}
